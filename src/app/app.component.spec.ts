49482f97f8a14df3c765d9f19d1e9674f2f64211
Tom Holland <code@thpro.net>
2020-05-06 08:42:55 -0700

    Adds App Title & Test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'cigna-challenge'`, () => {
+  it(`should have title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('cigna-challenge');
+    expect(app.title).toEqual(app.title);
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('cigna-challenge app is running!');
+    expect(compiled.querySelector('h1').textContent).toContain(compiled.title);
   });
 });
